4484ea21168d2ba44a75e3b26c91f2c83e8ff5ba
minericefield <minericefield@gmail.com>
2021-06-23 02:25:52 +0900

    inject and init them

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,56 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
   </div>
-  <router-view/>
 </template>
+
+<script lang="ts">
+import { defineComponent, provide, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { useGenerals, GENERALS_KEY } from '@/composables/useGenerals'
+import { useGraphicDesigns, GRAPHIC_DESIGNS_KEY } from '@/composables/useGraphicDesigns'
+import { useUiDesigns, UI_DESIGNS_KEY } from '@/composables/useUiDesigns'
+import { useDesigner, DESIGNER_KEY } from '@/composables/useDesigner'
+import { useOpening } from '@/composables/useOpening'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+  setup () {
+    const generals = useGenerals()
+    generals.registerRouteNav(useRouter())
+    provide(GENERALS_KEY, generals)
+    const graphicDesigns = useGraphicDesigns()
+    provide(GRAPHIC_DESIGNS_KEY, graphicDesigns)
+    const uiDesigns = useUiDesigns()
+    provide(UI_DESIGNS_KEY, uiDesigns)
+    const designer = useDesigner()
+    provide(DESIGNER_KEY, designer)
+    const { isOpeningVisible, onOpeningComplete } = useOpening(generals)
+
+    // Prefetch for smooth transition
+    graphicDesigns.fetch()
+    uiDesigns.fetch()
+    designer.fetch()
+
+    onMounted(() => {
+      generals.initialize()
+      window.addEventListener('resize', generals.initialize) // For browser debugger, changing userAgent. Actually updateWindowsInnerSize is enough
+    })
+
+    return {
+      generals,
+      graphicDesigns,
+      uiDesigns,
+      designer,
+      isOpeningVisible,
+
+      onOpeningComplete
+    }
+  }
+})
+</script>
 
 <style lang="scss">
 @import './styles/';
