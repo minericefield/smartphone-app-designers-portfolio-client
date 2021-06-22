f0bded1f5cff7a22ae9043936638fe865d24e368
minericefield <minericefield@gmail.com>
2021-06-23 00:55:47 +0900

    init style

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,10 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './styles/';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.app {
+  width: 100%;
+  height: 100%;
 }
 </style>
