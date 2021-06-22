3409a7a24090a87c0c20729da41bdbb3749e1c26
minericefield <minericefield@gmail.com>
2021-06-23 02:43:53 +0900

    add BgBehind

---
+++ src/components/BgBehind.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="bg-behind"
+  >
+    <bg-behind-image :content="content" />
+    <bg-rect />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Design, Designer } from 'smartphone-app-designers-portfolio-api-docs/client'
+
+import BgBehindImage from '@/components/BgBehindImage.vue'
+import BgRect from '@/components/BgRect.vue'
+
+export default defineComponent({
+  name: 'BgBehind',
+  components: {
+    BgBehindImage,
+    BgRect
+  },
+  props: {
+    content: {
+      type: Object as PropType<Design | Designer>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.bg-behind {
+  width: 100%;
+  height: 100%;
+  ::v-deep(.bg-rect) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 64%;
+  }
+  ::v-deep(.bg-behind-image) {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
